90d071b2c4439f4a89467f026b02d37dfd6d5560
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2014-12-08 14:13:12 +0900

    Initial Commit

---
+++ DynamicFunction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicFunction
+//
+//  Created by organlounge on 2014/12/08.
+//  Copyright (c) 2014年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
